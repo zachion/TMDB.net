47273a2e01a97531e1ab37259a3ca00e4f6533b0
Zach Ioannou <zioannou77@gmail.com>
2019-12-09 23:48:23 +0200

    added Result class

---
+++ TMDB.net/Class/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMDB.net.Class
+{
+    public class Result
+    {
+        public string profilePath { get; set; }
+        public bool adult { get; set; }
+        public int id { get; set; }
+        public List<KnownFor> knownFor { get; set; }
+        public string name { get; set; }
+        public double popularity { get; set; }
+    }
+}
