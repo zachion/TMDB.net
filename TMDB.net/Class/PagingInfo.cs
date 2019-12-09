38cc5a8879975fe6a4021eeddc8745d82fce643c
Zach Ioannou <zioannou77@gmail.com>
2019-12-09 23:47:24 +0200

    added PagingInfo class

---
+++ TMDB.net/Class/PagingInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMDB.net.Class
+{
+    public class PagingInfo
+    {
+        public int totalItems { get; set; }
+        public int itemsPerPage { get; set; }
+        public int currentPage { get; set; }
+        public int totalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)totalItems / itemsPerPage);
+            }
+        }
+    }
+}
