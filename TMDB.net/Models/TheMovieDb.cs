e6bbc29227232fd0420653b5e630fd06ed28611f
Zach Ioannou <zioannou77@gmail.com>
2019-12-09 23:27:07 +0200

    created the TheMovieDb model class

---
+++ TMDB.net/Models/TheMovieDb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TMDB.net.Models
+{
+    public class TheMovieDb
+    {
+        [Required]
+        public string searchText { get; set; }
+        public bool adult { get; set; }
+        public string alsoKnownAs { get; set; }
+        public string biography { get; set; }
+        public string birthday { get; set; }
+        public string deathday { get; set; }
+        public int gender { get; set; }
+        public string homePage { get; set; }
+        public int id { get; set; }
+        public string imdbId { get; set; }
+        public string name { get; set; }
+        public string placeOfBirth { get; set; }
+        public double popularity { get; set; }
+        public string profilePath { get; set; }
+    }
+}
