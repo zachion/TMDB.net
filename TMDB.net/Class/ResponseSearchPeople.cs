7cf74c6f2a7310757b701ab6a8042f493767a35d
Zach Ioannou <zioannou77@gmail.com>
2019-12-09 23:48:05 +0200

    added ResponseSearchPeople class

---
+++ TMDB.net/Class/ResponseSearchPeople.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMDB.net.Class
+{
+    public class ResponseSearchPeople
+    {
+        public int page { get; set; }
+        public List<Result> results { get; set; }
+        public int totalResults { get; set; }
+        public int totalPages { get; set; }
+    }
+}
