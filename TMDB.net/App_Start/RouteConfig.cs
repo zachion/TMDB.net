f8ddc0106ef0415de1231f2f0bb932cee1093dbb
Zach Ioannou <zioannou77@gmail.com>
2019-12-10 00:39:34 +0200

    added route configuration

--- TMDB.net/App_Start/RouteConfig.cs
+++ TMDB.net/App_Start/RouteConfig.cs
@@ -9,15 +9,31 @@ namespace TMDB.net
 {
     public class RouteConfig
     {
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                 name: "TmdbApi",
+                 url: "TmdbApi/{id}/",
+                 defaults: new { controller = "TmdbApi", action = "GetPerson", id = "" },
+                 constraints: new { id = @"^[0-9]+$" }
+             );
+
+            routes.MapRoute(
+                name: "TmdbApiPaging",
+                url: "TmdbApi/{peopleName}/{page}",
+                defaults: new { controller = "TmdbApi", action = "Index", peopleName = "", page = "" },
+                constraints: new { peopleName = @"^[a-zA-Z]+$", page = @"^[0-9]+$" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TmdbApi", action = "Index", id = UrlParameter.Optional }
             );
         }
+
     }
 }
