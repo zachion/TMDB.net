ee134d6641fbdbd84e7182e02321922832b8d79b
Zach Ioannou <zioannou77@gmail.com>
2019-12-18 23:58:51 +0200

    styled actor page

--- TMDB.net/App_Start/BundleConfig.cs
+++ TMDB.net/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace TMDB.net
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/actor").Include(
+                      "~/Content/actor.css"));
         }
     }
 }
